25a5cb45b335e6fdc912a195d85c81818edc7c82
francobatta <francobatta@outlook.com>
2021-04-20 07:21:19 -0300

    sass + lodash working ok

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
 module.exports = {
-  entry: './src/index.js', // aca podriamos usar code splitting
+  entry: "./src/index.js", // aca podriamos usar code splitting
   output: {
-    filename: 'main.js'
-  }
-}
+    filename: "main.js",
+  },
+  module: {
+    // loader: objeto que contiene los "metodos" use y test. sabe procesar archivos que no sean js
+    rules: [
+      {
+        test: /\.scss$/, // que archivos puedo load con el siguiente use?
+        use: [ // loaders a aplicar
+          'style-loader', // CSS al DOM
+          'css-loader', // convierte CSS a CommonJS
+          'sass-loader' // compila Sass a CSS
+        ]
+      },
+    ],
+  },
+};
